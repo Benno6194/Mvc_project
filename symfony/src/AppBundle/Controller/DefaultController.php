eb436810a321945e77a2f74c674add70f7cd5313
Benno Eikelenboom <benno6194@gmail.com>
2016-10-08 19:37:25 +0200

    omdat ik een tijdje vergeten ben te committen, nu een iets grotere commit met aanpassingen die ik heb gedaan. nu veel bezig met het opzetten van het systeem zoals de bezoeker het hoort te kunnen zien

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -5,24 +5,38 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $repo = $this->getDoctrine()->getRepository("AppBundle:Tasks");
+        $qTC = $repo->createQueryBuilder('T');
+        $qTC->select('COUNT(T)');
+        $qTC->where('T.taskCheck = :check');
+        $qTC->setParameter('check', 0);
+
+        $taskCounter = $qTC->getQuery()->getSingleScalarResult();
+
+        $tasks = $repo->findAll();
+        return $this->render('default/index.html.twig', array('taskCount'=>$taskCounter, 'tasks'=>$tasks));
+
+        /* replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
         ]);
+        */
     }
 
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin/", name="admin")
      */
-    public function adminAction ()
+    public function adminAction()
     {
+        return $this->render('admin/admin.html.twig');
     }
 }
