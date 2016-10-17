fbc0b704776be0195cbcb4094de0fa7d3f1483cc
Benno Eikelenboom <benno6194@gmail.com>
2016-10-17 12:44:50 +0200

    .

--- symfony/src/AppBundle/Controller/ProfileController.php
+++ symfony/src/AppBundle/Controller/ProfileController.php
@@ -4,7 +4,26 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity;
 
 class ProfileController extends Controller
 {
+    /**
+     * @Route("/profile")
+     * @Route("/profile/settings", name="myProfile")
+     */
+    public function myProfileAction()
+    {
+        $user = $this->getUser();
+        $userId = $user->getId();
+
+        $repoU = $this->getDoctrine()->getRepository("AppBundle:User");
+        $queryUser = $repoU->createQueryBuilder('U');
+        $queryUser->where('U.id = :id');
+        $queryUser->setParameter('id', $userId);
+
+        $userInfo = $queryUser->getQuery()->getResult();
+
+        return $this->render('profile/myProfile.html.twig', array('userInfo'=>$userInfo));
+    }
 }
